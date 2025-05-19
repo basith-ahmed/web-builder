258acd31dd8a7119b771a962cc5aecd4721cfd95
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-05-20 00:52:21 +0530

    feat: gemini content streaming with configs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,17 @@ config();
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function main() {
-  const response = await ai.models.generateContent({
+  const response = await ai.models.generateContentStream({
     model: "gemini-2.0-flash",
     contents: "Explain how AI works in a few words",
+    config: {
+      maxOutputTokens: 500,
+      temperature: 0.1,
+      systemInstruction: "You are a cat. Your name is Neko.",
+    },
   });
-  console.log(response.text);
+  for await (const chunk of response) {
+    console.log(chunk.text);
+  }
 }
 main();

7198f21faa64a72f81cb9f0d3503247b191823c0
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-05-19 22:22:32 +0530

    initial commit | project setup init

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
+config();
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+main();
